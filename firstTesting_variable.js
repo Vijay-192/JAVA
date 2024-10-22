06cea54debedfedaa4fe5bdbdb2cb575cc1dd991
VIJAY <vsaini47428@gmail.com>
2024-10-22 14:51:23 +0530

    test

---
+++ firstTesting_variable.js
@@ -0,0 +1,48 @@
+// let Bankbranch=2890;
+// console.log(typeof Bankbranch);
+
+// var HowToWin_Friends="[SouravTomar,GouravTomar,HiteshKumar,HarshVerma,HarshBharti, etc]";
+// console.log(HowToWin_Friends);
+// console.log(typeof [HowToWin_Friends]);
+// console.log(indexedDB );
+
+// ===============    DATA TYPE FOR JAVASCRIPT ====================//
+// string 
+// let  type= "3442-bank of span for professor/burlin";
+// console.table(type);
+// console.log(typeof type);
+// var user_name="hitesh\nKumar";
+// console.log(user_name);
+// var Roll_Number_Of_University= 22011002185;
+// console.log(Roll_Number_Of_University);
+// console.log(typeof Roll_Number_Of_University);
+// let Logitech_Wired=MNP;
+// console.log(Logitech_Wired); 
+
+// console.log(typeof undefined);
+// undefined is type of error show in  a javascript
+
+// null data type => object
+// console.log(typeof null);
+
+// conversion kisa hota ha 
+
+// console.log( null> 0 );
+// console.log( null>= 02 );
+// console.log( null< 1 );
+// console.log( null> 0 );
+
+
+// console.log('2' === 2);
+
+// const id = Symbol("##$");
+// const account_id = Symbol("37$");
+// console.log(id===account_id);
+// const Big_Number = 45678962021.36382;
+// console.log(typeof Big_Number);
+
+// console.log(`i am here ${ 'this nowup '}`);
+
+// let name1=GameGradionPubgMode;
+// console.log(name1.length);
+
